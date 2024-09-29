1f9634d5a78fdad08ea99a48cb65d564e6744bbe
Muralisanku <sankumurali4a6@gmail.com>
2024-09-29 18:44:01 +0530

    testing variables again

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=1$
+PERSON2=2$
+
+echo "$PERSON1: Hello $PERSON2, Good morning"
+echo "$PERSON2: Hi $PERSON1, very good morning"
+echo "$PERSON1: Thanks, how are you"
+echo "$PERSON2: Im good, how are you"
+
