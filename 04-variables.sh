7088c8b0b6d50c367e0303e2348df7ae015f0f6c
Muralisanku <sankumurali4a6@gmail.com>
2024-12-05 21:38:43 +0530

    edit the names

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hello $PERSON2, Good Morning"
+echo "$PERSON2: Very good Morning, How are you"
+echo "$PERSON1: Thanks, What are you doing"
+echo "$PERSON2: Im doing well, how about you"
