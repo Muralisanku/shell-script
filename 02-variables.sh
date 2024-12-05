2f8b93eaba889852a9b7452c0a5666d03334fe49
Muralisanku <sankumurali4a6@gmail.com>
2024-12-05 21:16:32 +0530

    adding 2nd push

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hello Suresh, Good Morning"
+echo "Suresh: Very good Morning, How are you"
+echo "Ramesh: Thanks, What are you doing"
+echo "Suresh: Im doing well, how about you"
