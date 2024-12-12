d5db84795de217b8a403780bd34181ca3a300fee
Muralisanku <sankumurali4a6@gmail.com>
2024-12-12 20:47:10 +0530

    Installing packages

--- 12-install-packages.sh
+++ 12-install-packages.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 if [ $ID -ne 0 ]
     then    
-        echo "ERROR:: Please run this script with root access"
+        echo -e "$R ERROR:: Please run this script with root access $N"
         exit 1
     else
         echo "You are root user"
