21b3ef93874dc75a573421d92797c7c6e45727bf
Muralisanku <sankumurali4a6@gmail.com>
2024-12-12 20:40:50 +0530

    Installing packages

--- 11-logs.sh
+++ 11-logs.sh
@@ -4,6 +4,9 @@ ID=$(id -u)
 
 echo "script name: $0"
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
---
+++ 12-install-packages.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+    then    
+        echo "ERROR:: Please run this script with root access"
+        exit 1
+    else
+        echo "You are root user"
+    fi
+
+    echo "All Arguments passed: $@"
