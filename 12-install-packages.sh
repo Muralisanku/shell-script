0c2eefb70a266e6a215aa5f2ef85cd6037ebd07e
Muralisanku <sankumurali4a6@gmail.com>
2024-12-12 21:08:37 +0530

    Installing packages

--- 12-install-packages.sh
+++ 12-install-packages.sh
@@ -3,7 +3,21 @@
 ID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+}
 
 if [ $ID -ne 0 ]
     then    
@@ -13,4 +27,16 @@ if [ $ID -ne 0 ]
         echo "You are root user"
     fi
 
-    echo "All Arguments passed: $@"
+    # echo "All Arguments passed: $@"
+
+    for package in $@
+    do
+        yum list installed $package &>> $LOGFILE
+        if [ $? -ne 0 ]
+        then
+            yum install $package -y &>> $LOGFILE
+            VALIDATE $? "Installation of $package"
+        else 
+            echo -e "$package is already installed ... $Y SKIPPING $N"
+        fi
+    done
