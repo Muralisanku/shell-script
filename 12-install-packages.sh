2d394f74bd8cf7ccecf7932570505546acfaa886
Muralisanku <sankumurali4a6@gmail.com>
2024-12-30 19:53:49 +0530

    testing

--- 12-install-packages.sh
+++ 12-install-packages.sh
@@ -4,7 +4,16 @@ ID=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+    else
+        echo -e "$2 .... $G SUCCESS $N"
+}
 
 if [ $ID -ne 0 ]
 then
@@ -14,5 +23,18 @@ else
     echo -e "$G you are root user $N"
 fi
 
-echo "All arguments passed :: $@"
+#echo "All arguments passed :: $@"
+
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then
+        yum install $package -y
+        VALIDATE $? "Installation of $package"
+    else
+        echo -e "$package is already installed .. $Y SKIPPING $N"
+    fi
+done
+
 
