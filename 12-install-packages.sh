a67a1861d8f953574e125280b60e5fc6216ccfe8
Muralisanku <sankumurali4a6@gmail.com>
2024-12-30 11:09:04 +0530

    testing

--- 12-install-packages.sh
+++ 12-install-packages.sh
@@ -2,12 +2,16 @@
 
 ID=$(id -u)
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 if [ $ID -ne 0 ]
 then
-    echo "Please run the script with root access"
+    echo -e " $R Please run the script with root access $N"
     exit 1
 else
-    echo "you are root user"
+    echo "$G you are root user $N"
 fi
 
 echo "All arguments passed :: $@"
