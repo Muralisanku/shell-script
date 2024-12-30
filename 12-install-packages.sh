faa3d40ffa2b4f8de7998f8cd1776e0b78274e41
Muralisanku <sankumurali4a6@gmail.com>
2024-12-30 11:03:31 +0530

    testing

--- 12-install-packages.sh
+++ 12-install-packages.sh
@@ -1,43 +1,14 @@
 #!/bin/bash
 
 ID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
-
-echo "script started executing at $TIMESTAMP" &>> $LOGFILE
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILED $N"
-    else
-        echo -e "$2 ... $G SUCCESS $N"
-    fi
-    }   
 
 if [ $ID -ne 0 ]
-    then    
-        echo -e "$R ERROR:: Please run this script with root access $N"
-        exit 1
-    else
-        echo "You are root user"
-    fi
+then
+    echo "Please run the script with root access"
+    exit 1
+else
+    echo "you are root user"
+fi
 
-    # echo "All Arguments passed: $@"
+echo "All arguments passed :: $@"
 
-    for package in $@
-    do
-        yum list installed $package &>> $LOGFILE
-        if [ $? -ne 0 ]
-        then
-            yum install $package -y &>> $LOGFILE
-            VALIDATE $? "Installation of $package"
-        else 
-            echo -e "$package is already installed ... $Y SKIPPING $N"
-        fi
-    done
