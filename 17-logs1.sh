ce324f1f9ad20c9eacb2cf21871548b8c1ca7d71
Muralisanku <sankumurali4a6@gmail.com>
2025-01-30 22:41:05 +0530

    testing

--- 17-logs1.sh
+++ 17-logs1.sh
@@ -2,15 +2,18 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
         if [ $1 -ne 0 ]
     then
-        echo "$2 ... failed"
+        echo "$2 ... $R failed $N "
         exit 1
     else
-        echo "$2 ... SUCCESS"
+        echo "$2 ... $G SUCCESS $N "
     fi
 }
 if [ $ID -ne 0 ]
