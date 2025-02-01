86de6446d7b96cb13193619da4d730a743aee988
Muralisanku <sankumurali4a6@gmail.com>
2025-02-01 21:03:40 +0530

    Args paased

--- 18-install-packages1.sh
+++ 18-install-packages1.sh
@@ -5,6 +5,14 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e  "$2 .... $R FAILED $N"
+    else
+        echo -e  "$2 .... $G FAILED $N"
+}
+
 if [ $ID -ne 0 ]
 then
     echo -e "$R ERROR:: Please run this script with Root access $N"
@@ -13,5 +21,14 @@ else
     echo "You are Root user"
 fi
 
-echo "All Args passed: $@"
+#echo "All Args passed: $@"
 
+for package in $@
+do
+    yum list installed $package
+    if [ $? -ne 0 ]
+    then
+    yum install $package -y
+    VALIDATE $? "Installation of $package"
+
+done
